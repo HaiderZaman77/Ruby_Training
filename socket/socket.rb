a56c3f3dbe51ec1d22025a187eeb5b31d02d621c
Haider Zaman <haider.zaman@coeus-solutions.de>
2021-04-30 16:42:55 +0500

    Socket programming

---
+++ socket/socket.rb
@@ -0,0 +1,13 @@
+#first run the server file then the socket
+
+require 'socket'        # Sockets are in standard library
+
+hostname = 'localhost'
+port = 2000
+
+s = TCPSocket.open(hostname, port)
+
+while line = s.gets     # Read lines from the socket
+   puts line.chop       # And print with platform line terminator
+end
+s.close      
