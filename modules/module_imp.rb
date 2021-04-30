f84e1b7475d9f5d88aae5bf8c71112bc2401c91b
Haider Zaman <haider.zaman@coeus-solutions.de>
2021-04-30 17:15:53 +0500

    trying to add another module method

--- modules/module_imp.rb
+++ modules/module_imp.rb
@@ -3,12 +3,16 @@ $LOAD_PATH<<'.'
 require "modules"
 class Calculation
 include Calculator
+
 def value
 var = gets.to_i
 var1 = gets.to_i
 result = Calculator.add(var,var1)
+puts "Multiplication"
+result1 = Calculator.mul(var,var1)
 end
+
 end
 obj1 = Calculation.new
-puts obj1.value
+obj1.value
 
--- modules/modules.rb
+++ modules/modules.rb
@@ -3,6 +3,9 @@ module Calculator
 #input1 = gets.to_i
 #input2 = gets.to_i
 def Calculator.add(var, var1)
-var + var1
+puts var + var1
+end
+def Calculator.mul(var, var1)
+puts var * var1
 end
 end
