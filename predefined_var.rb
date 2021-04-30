ef2ff0a6937de9786efe7f3aaf5dcca22fe7bd74
Haider Zaman <haider.zaman@coeus-solutions.de>
2021-04-30 16:47:39 +0500

    Predefined varables

---
+++ predefined_var.rb
@@ -0,0 +1,27 @@
+include Math
+number = 5
+arr= [1,2,3,4,5]
+puts ldexp(2,number) 
+str = sprintf("%s\n", "abc")   
+puts str 
+
+str = sprintf("d=%d", 42)      
+puts str 
+
+str = sprintf("%04x", 255)     
+puts str 
+
+str = sprintf("%8s", "hello")  
+puts str 
+
+str = sprintf("%.2s", "hello")
+puts str 
+
+puts test(?r, "threads.rb" )  
+puts test(?X, "threads.rb" )   
+puts test(?w, "test1.rb" ) 
+puts $.
+puts $0
+puts $$
+puts RUBY_VERSION
+puts #{ARGF}
